61dd2818caead54b5445c064f32a6fe21aa47b9e
gshanko125298 <gdekebo2020@gmail.com>
2022-08-16 14:40:57 +0300

    update

--- tests/Extraacted_data _file.py
+++ tests/Extraacted_data _file.py
@@ -136,3 +136,17 @@ if __name__ == "__main__":
     _, tweet_list = read_json("data/global_twitter_data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df(save=True) 
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
